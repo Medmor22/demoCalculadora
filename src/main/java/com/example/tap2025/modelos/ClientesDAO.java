c0b80912ba84ebb54bc418551c8fdb833d13490e
Carmen Arreguin <car252004@gmail.com>
2025-02-28 11:53:07 -0600

    Diseño de tabla

--- src/main/java/com/example/tap2025/modelos/ClientesDAO.java
+++ src/main/java/com/example/tap2025/modelos/ClientesDAO.java
@@ -1,5 +1,11 @@
 package com.example.tap2025.modelos;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ClientesDAO {
     private int idCte;
     private String nomCte;
@@ -48,19 +54,65 @@ public class ClientesDAO {
     }
 
     public void INSERT(){
-        String query = "INSERT INTO clientes(nomCte, telCte, direccion, emailCte)" +
-                "values('"+nomCte+"', '"+telCte+"', '"+direccion+"', '"+emailCte+"')";
+        //String query = "INSERT INTO clientes VALUES(?,?,?,?,?)";
+        //String sql = "insert into clientes values(?,?,?,?,?,?)";
+        String query = "INSERT INTO clientes(nomCte, telCte, direccion, emailCte) " +
+                "VALUES('"+nomCte+"','"+telCte+"','"+direccion+"','"+emailCte+"')";
+        try{
+            Statement stmt = conexion.connection.createStatement();
+            stmt.executeUpdate(query);
+        }catch (Exception e){
+            //trasa de ejecución
+            e.printStackTrace();
+        }
     }
 
     public void UPDATE(){
-
+        String query = "UPDATE clientes SET nomCte = '"+nomCte+"', " +
+                "telCte = '"+telCte+"', direccion = '"+direccion+"', " +
+                "emailCte = ' "+emailCte+"' WHERE idCliente = "+idCte+";";
+        try{
+            Statement stmt = conexion.connection.createStatement();
+            stmt.executeUpdate(query);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void DELETE(){
+        String query = "DELETE FROM clientes WHERE idCliente = "+idCte+";";
 
+        try{
+            Statement stmt = conexion.connection.createStatement();
+            stmt.executeUpdate(query);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public void SELECT(){
+    //Pasar los registros (elementos) de la BD a Java a un conjunto de objetos para que pueda entenderlos
+    //ObservableList tipo de objeto que requiere el componente que usaremos para mostrarlo en pantalla
 
+    public ObservableList<ClientesDAO> SELECT(){
+        String query = "SELECT * FROM clientes"; //Consulta que te trae todos los resultados de la tabla
+        ObservableList<ClientesDAO> ListaC = FXCollections.observableArrayList();
+        ClientesDAO objC;
+
+        try{
+            Statement stmt = conexion.connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                objC = new ClientesDAO();
+                objC.setIdCte(rs.getInt("idCliente"));
+                objC.setNomCte(rs.getString("nomCte"));
+                objC.setTelCte(rs.getString("telCte"));
+                objC.setDireccion(rs.getString("direccion"));
+                objC.setEmailCte(rs.getString("emailCte"));
+                ListaC.add(objC);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return ListaC;
     }
 }
---
+++ src/main/java/com/example/tap2025/vistas/ListaClientes.java
@@ -0,0 +1,63 @@
+package com.example.tap2025.vistas;
+
+import com.example.tap2025.modelos.ClientesDAO;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ListaClientes extends Stage {
+
+    private ToolBar tblMenu;
+    private TableView<ClientesDAO> tbvClientes;
+    private VBox vBox;
+    private Scene escena;
+    private Button btnAgregar;
+
+    public ListaClientes() {
+        CrearUI();
+        this.setTitle("Lista de Clientes");
+        this.setScene(escena);
+        this.show();
+    }
+
+    private void CrearUI() {
+        btnAgregar = new Button(); //agrega una imagen, iconfinder
+        //btnAgregar.setGraphic(new ImageView(getClass().getResource("/images/agregar.png").toString()));
+        ImageView imv = new ImageView(getClass().getResource("/images/211872_person_add_icon.png").toString());
+        /*
+        ImageView imv = new ImageView(getClass().getResource("/images/....png)
+        imv.setFitWidth(20);
+        imv.setFitHeight(20);
+        * */
+        btnAgregar.setGraphic(imv);
+        tblMenu = new ToolBar(btnAgregar);
+        tbvClientes = new TableView<>();
+        CreateTable();
+
+        vBox = new VBox(tblMenu, tbvClientes);
+        escena = new Scene(vBox, 800, 600);
+
+    }
+
+    private void CreateTable() {
+        TableColumn<ClientesDAO,String> tbcNomCte = new TableColumn<>("Nombre");
+        tbcNomCte.setCellValueFactory(new PropertyValueFactory<>("nomCte"));
+
+        TableColumn<ClientesDAO,String> tbcDireccion = new TableColumn<>("Dirección");
+        tbcDireccion.setCellValueFactory(new PropertyValueFactory<>("direccion"));
+
+        TableColumn<ClientesDAO,String> tbcTelefono = new TableColumn<>("Telefono");
+        tbcTelefono.setCellValueFactory(new PropertyValueFactory<>("telCte"));
+
+        TableColumn<ClientesDAO,String> tbcEmail = new TableColumn<>("Email");
+        tbcEmail.setCellValueFactory(new PropertyValueFactory<>("emailCte"));
+    }
+
+
+}
